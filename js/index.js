bb433e91563d44b1d2d92f5e8ed08d2a54311f29
xkwg <xkwg@163.com>
2017-10-19 00:53:27 +0900

    1

--- js/index.js
+++ js/index.js
@@ -20,16 +20,16 @@ $(function(){
 					}, 3000)
 					setInterval(function() {
 					    $(".p3 .p3_4").css({"animation" : "word 1s", "opacity" : "1"})
-					}, 3000);
+					}, 4000);
 					setInterval(function(){
 						typing("p3_4", p3_str3)
 					}, 5000)
 					setInterval(function() {
 					    $(".p3 .p3_6").css({"animation" : "word 1s", "opacity" : "1"})
-					}, 5000);
+					}, 6000);
 					setInterval(function() {
 					    $(".p3 .p3_5").css({"animation" : "word 1s", "opacity" : "1"})
-					}, 6000);
+					}, 7000);
     				break;
     			case 4:
     				$(".p5 .portrait").css({"animation" : "portrait_p5 1s", "left" : "425px"})
@@ -37,6 +37,31 @@ $(function(){
     				setInterval(function(){
     					$(".p5 .p5_2").css({"animation" : "word 1s", "opacity" : "1"})
     				}, 1000)
+    				break;
+    			case 5:
+    				$(".p6 .p6_1").css({"animation" : "word 1s", "opacity" : "1"})
+    				setInterval(function(){
+    					$(".p6 .p6_3").css({"animation" : "word 1s", "opacity" : "1"})
+    				}, 1000)
+    				break;
+    			case 6:
+					$(".p7 .p7_1").css({"animation" : "word 1s", "opacity" : "1"})
+	    			setInterval(function() {
+					    $(".p7 .p7_2").css({"animation" : "word 1s", "opacity" : "1"})
+					    typing("p7_2", p7_str1)
+					}, 1000);
+    				setInterval(function() {
+					    $(".p7 .p7_3").css({"animation" : "word 1s", "opacity" : "1"})
+					}, 2000);
+					setInterval(function(){
+						typing("p7_3", p7_str2)
+					}, 3000)
+					setInterval(function() {
+					    $(".p7 .p7_4").css({"animation" : "word 1s", "opacity" : "1"})
+					}, 4000);
+					setInterval(function(){
+						typing("p7_4", p7_str3)
+					}, 5000)
     				break;
 
     		}
@@ -50,11 +75,20 @@ $(function(){
     $(".p4_exit").click(function(){
     	$(".p4").css("display", "none")
     })
+
+    $(".p6_3").click(function(){
+    	$(".p6 .p6_2").css({"animation" : "word 1s", "opacity" : "1"})
+    })    
 });
 
 var p3_str1 = 'Gavin:</br>请问您有什么财富目标？'
 var p3_str2 = "林先生:</br>我希望在我女儿18岁生日的时候</br>能够为她提供留学资金。"
 var p3_str3 = "Gavin:</br>了解，根据您的需求，请看pad上</br>我为您度身定做的资产配置规划。"
+
+var p7_str1 = "Julie:</br>顺利抵达美国，花旗账户</br>已经开好啦～"
+var p7_str2 = "林先生:</br>好的宝贝。我刚刚把生活费</br>打到你账上了。遇到什么问</br>题随时和我或你妈妈说。"
+var p7_str3 = "Julie:</br>转账收到！谢谢爸爸！"
+
 var i = 0;  
 
 function typing(divname, str){
