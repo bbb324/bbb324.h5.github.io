58e020b3d1904f3c4eb2a1242921fcbabf18bf00
bbb324 <johnigzie@163.com>
2017-10-19 00:22:54 +0800

    修改js时间顺序，修改部分样式

--- js/index.js
+++ js/index.js
@@ -6,30 +6,30 @@ $(function(){
     				$(".p2 .p2_word").css({"animation" : "word 1s", "opacity" : "1"})
     				break;
     			case 3:
-    				$(".p3 .portrait").css({"animation" : "portrait 1s", "left" : "263px"})
+    				$(".p3 .portrait").css({"animation" : "portrait 1s", "left" : "35.2vw"})
 					$(".p3 .p3_1").css({"animation" : "word 1s", "opacity" : "1"})
-	    			setInterval(function() {
+	    			setTimeout(function() {
 					    $(".p3 .p3_2_bg").css({"animation" : "word 1s", "opacity" : "1"})
-					    typing("p3_2", p3_str1)
+					    typing("p3_2", p3_str1, 0)
 					}, 1000);
-    				setInterval(function() {
+					setTimeout(function() {
 					    $(".p3 .p3_3_bg").css({"animation" : "word 1s", "opacity" : "1"})
-					}, 2000);
-					setInterval(function(){
-						typing("p3_3", p3_str2)
-					}, 3000)
-					setInterval(function() {
+					}, 3500);
+					setTimeout(function(){
+						typing("p3_3", p3_str2, 0)
+					}, 4000)
+					setTimeout(function() {
 					    $(".p3 .p3_4_bg").css({"animation" : "word 1s", "opacity" : "1"})
-					}, 3000);
-					setInterval(function(){
-						typing("p3_4", p3_str3)
-					}, 5000)
-					setInterval(function() {
+					}, 7000);
+					setTimeout(function(){
+						typing("p3_4", p3_str3, 0)
+					}, 8000)
+					setTimeout(function() {
 					    $(".p3 .p3_6").css({"animation" : "word 1s", "opacity" : "1"})
-					}, 5000);
-					setInterval(function() {
+					}, 13000);
+					setTimeout(function() {
 					    $(".p3 .p3_5").css({"animation" : "word 1s", "opacity" : "1"})
-					}, 6000);
+					}, 14000);
     				break;
 
     		}
@@ -46,21 +46,21 @@ $(function(){
 });
 
 var p3_str1 = 'Gavin:</br>请问您有什么财富目标？'
-var p3_str2 = "我希望在我女儿18岁的时候</br>能够为她提供留学资金。"
-var p3_str3 = "Gavin:</br>了解，根据您的需求，请看pad上</br>我为您量身定做的资产配置规划。"
-var i = 0;  
+var p3_str2 = '我希望在我女儿18岁的时候</br>能够为她提供留学资金。'
+var p3_str3 = 'Gavin:</br>了解，根据您的需求，请看pad上</br>我为您量身定做的资产配置规划。'
 
-function typing(divname, str){
-	// alert(div + str)
-    var div = document.getElementById(divname);  
+
+function typing(divname, str, i){
+    var div = document.getElementById(divname);
+
     if (i <=  str.length) {  
         div.innerHTML = str.slice(0, i++) + '_';  
         // setTimeout("typing()", 200);//閫掑綊璋冪敤 
         setTimeout(function(){
-        	typing(divname, str)
+        	typing(divname, str, i)
         }, 100)
     }  
-    else{  
+    else{
         div.innerHTML = str;//缁撴潫鎵撳瓧,绉婚櫎 _ 鍏夋爣  
     }  
 }
